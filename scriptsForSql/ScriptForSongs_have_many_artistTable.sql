501494639a93a6513442dd36f6d752f8abcf2978
derekyenser <60159119+derekyenser@users.noreply.github.com>
2020-04-19 15:26:18 -0400

    Delete ScriptForSongs_have_many_artistTable.sql

--- scriptsForSql/ScriptForSongs_have_many_artistTable.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-
-DROP TABLE IF EXISTS song_have_many_artist;
-
-CREATE TABLE song_have_many_artist (
-artist_name VARCHAR(45) REFERENCES Artists(artist_name),
-song_num INTEGER REFERENCES Songs(song_num),
-PRIMARY KEY (artist_name, song_num));
