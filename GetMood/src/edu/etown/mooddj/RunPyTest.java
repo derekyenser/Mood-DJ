6d9108650838c7f32e39cb2b454521cec2a82d34
derekyenser <60159119+derekyenser@users.noreply.github.com>
2020-04-20 20:17:13 -0400

    Update RunPyTest.java

--- GetMood/src/edu/etown/mooddj/RunPyTest.java
+++ GetMood/src/edu/etown/mooddj/RunPyTest.java
@@ -1,9 +1,10 @@
-package edu.etown.mooddj;
+import java.io.*;
 
 class runPyTest{
 	public static void main(String a[]){
 		RunPy script = new RunPy("PythonScript\\dist\\pullSavedSongs.exe");
-		script.getDBCreds(); // Optional, if you have a DB set up on your computer you will want to use this
+		script.getUsername("The email adress of the user's spotify account");
+		script.getDBCreds(); // The addres of the databse is in the code, so this line is unecessary
 		script.run();
 	}
 }
